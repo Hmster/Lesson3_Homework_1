71b3db284d8de1d9097522738198845a6ee5dfbe
Aleksandr Stepanov <alex_stepanoff@inbox.ru>
2022-03-08 23:07:00 +0300

    Make p.1 of Homework(make class, method)

---
+++ Lesson3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_Homework_1
+{
+    internal static class Lesson3
+    {   
+        /// <summary>
+        /// Make array and it's diagonal
+        /// </summary>
+        /// <returns>array</returns>
+        internal static int[,] MakeArray(int[,] array)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(1,9);
+                }
+            }
+            return array;
+        }
+
+        internal static int[,] MakeDiag(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == j)
+                        array[i, j] = array[i, j];
+                    else
+                        array[i, j] = 0;
+                }
+            }
+            return array;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,34 @@ namespace Lesson3_Homework_1
     {
         static void Main(string[] args)
         {
-            //There will be homework
+            int[,] array = new int[5, 5];
+
+            Console.WriteLine("Fill array with elements:");
+
+            Lesson3.MakeArray(array);
+            ArrayPrint(array);
+
+            Console.WriteLine("\nAnd now, show only diagonal of array:\n");
+
+            Lesson3.MakeDiag(array);
+            ArrayPrint(array);
+
+            
+
+            Console.ReadKey();
+        }
+
+        internal static int[,] ArrayPrint(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0,3}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            return array;
         }
     }
 }
